e6b7c05349425be635387b8385c65871128faf37
SrAdrielM <20230542@ricaldone.edu.sv>
2025-05-21 12:10:08 -0600

    acabe...

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,26 +1,30 @@
 import React from "react"
 import "./navbar.css";
 
+import img1 from "../imgs/logoZ.jpg"
+import img2 from "../imgs/products.png"
+import img3 from "../imgs/customers.png"
+import img4 from "../imgs/employees.png"
+
 const Nav = () => {
     return (
         <nav>
             <div id="bar">
-                <div>MyApp</div>
+                <div>
+                    <img src={img1} id="logo"/>
+                </div>
                 <ul>
                     <li>
-                        <a href="/">
-                            Productos
-                        </a>
+                        <a href="/">Productos
+                        <img src={img2}/></a>
                     </li>
                     <li>
-                        <a href="/Customers">
-                            Clientes
-                        </a>
+                        <a href="/Customers">Clientes
+                        <img src={img3}/></a>
                     </li>
                     <li>
-                        <a href="/Employees">
-                            Empleados
-                        </a>
+                        <a href="/Employees">Empleados
+                        <img src={img4}/></a>
                     </li>
                 </ul>
             </div>
